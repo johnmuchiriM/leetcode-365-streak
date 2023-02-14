a2c861a3dd474d2d845528bc05fa30149dd9e971
johnmuchirim <jeanfrancaiseharicot@gmail.com>
2023-02-14 20:49:45 +0300

    update: number-of-segments-in-a-string

---
+++ feb14number-of-segments-of-a-string/number-of-segments-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSegments(String s) {
+        String trimmed = s.trim();
+
+        if(trimmed.equals("")){
+            return 0;
+        }
+        return trimmed.split("\\s+").length;
+        
+    }
+}
